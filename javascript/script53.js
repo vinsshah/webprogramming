55f483ff92d5e7f324298d320736fa2d1fb3b79e
vinsshah <vins.shah1997@gmail.com>
2017-03-08 11:39:14 +0530

    Create script53.js

---
+++ javascript/script53.js
@@ -0,0 +1,24 @@
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += " & Lovin' it!";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+}
